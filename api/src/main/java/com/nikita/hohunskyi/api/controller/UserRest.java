01f50839a5985c62c309142e34015aac66e1a926
Nikita <nikita.gogunskii@gmail.com>
2020-07-16 14:00:05 +0300

    Fixed modules dependencies

---
+++ api/src/main/java/com/nikita/hohunskyi/api/controller/UserRest.java
@@ -0,0 +1,26 @@
+package com.nikita.hohunskyi.api.controller;
+
+import com.nikita.hohunskyi.domain.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User rest controller
+ */
+@RestController
+@Validated
+public class UserRest {
+
+
+    @Autowired
+    public UserRest() {
+
+    }
+
+    @GetMapping("/user")
+    UserDto all() {
+        return new UserDto();
+    }
+}
